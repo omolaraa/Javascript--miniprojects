5438f16c46c5f5089572f347488e9c999ac82263
omolaraa <omolarabiz19@gmail.com>
2021-06-22 17:19:33 +0100

    fourth commit

--- playingCard.js
+++ playingCard.js
@@ -1,12 +1,22 @@
-function pick(arr) {
- const index = Math.floor(Math.random() * arr.length);
- return arr[index];
-}
 function getCard() {
  const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'K', 'A'];
- const value = pick(values)
+ const valueIdx = Math.floor(Math.random() * values.length)
+ const value = values[valueIdx]
  const suits = ['club', 'spades', 'hearts', 'diamonds']
- const suit = pick(suits)
- console.log ({value: value, suit: suit})
+ const suitIdx = Math.floor(Math.random() * suits.length)
+ const suit = suits[suitIdx]
+ console.log({value: value, suit: suit})
 }
 getCard()
+
+// Method 2
+function pick(arr) {
+ const index = Math.floor(Math.random() * arr.length)
+ return arr[index];
+}
+function getCard2() {
+ const values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'K', 'A'];
+ const suits = ['club', 'spades', 'hearts', 'diamonds']
+ console.log({value: pick(values), suit: pick(suits)})
+}
+getCard2()
